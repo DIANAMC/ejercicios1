0f506cac474a6b7403df52fabc1c7e7e1d397d32
Marcela Camacho <marcelacamacho@MacBook-Air-de-Marcela.local>
2020-04-06 16:46:09 -0500

    ejercicio mayorque

--- Ejercicio 7/index.js
+++ Ejercicio 7/index.js
@@ -1,1 +1,26 @@
-document.write("El resultado de suma 3 + 5 es 8")
+var n1 = prompt("ingrese un numero")
+var n2 = prompt("ingrese otro numero")
+
+n1 = parseInt(n1);
+n2 = parseInt(n2);
+console.log(n1,n2)
+
+function mayorQue(n1,n2){
+    console.log(n1)
+    console.log(n2)
+    if(n1 == n2){
+        alert("es el mismo numero")
+    }
+    if(n1 > n2){
+        alert( n1 +" es mayor que "+n2)
+
+    }
+    if(n2>n1){
+        alert(n2 + "es mayor que" + n1)
+    }
+    else{
+        alert("ingrese datos validos")
+    }
+}
+
+mayorQue(n1,n2)
