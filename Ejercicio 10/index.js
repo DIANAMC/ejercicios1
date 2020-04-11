bdaf994aedc7ffe6046d4b4d1b780478eea36a54
Marcela Camacho <marcelacamacho@MacBook-Air-de-Marcela.local>
2020-04-10 20:06:13 -0500

    ejercicio 10

--- Ejercicio 10/index.js
+++ Ejercicio 10/index.js
@@ -1,1 +1,19 @@
-document.write("El resultado de suma 3 + 5 es 8")
+var frase =[];
+frase = prompt("Ingresa tu frase");
+
+var caract = frase.length;
+var i = 0;
+var cont= 0;
+console.log(caract)
+console.log(i)
+function conteoA(frase){
+    for(i=0; i<=caract; i++){
+        if(frase[i]== "a"){
+            cont++
+        }
+
+    }
+    alert("la letra a se encuentra " + cont + " veces")
+}
+
+conteoA(frase)
